8e0245c75ee713e7f36c474693defd0f714d5730
Prince Narteh <prinarteh@gmail.com>
2025-08-07 18:58:56 +0000

    feat: added cors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,7 @@ import (
 
 	chi "github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 
 	"github.com/PrinceNarteh/social/internal/config"
 	"github.com/PrinceNarteh/social/internal/utils"
@@ -19,6 +20,15 @@ func NewApplication() *application {
 
 func (app *application) mount() *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
